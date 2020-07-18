a818e346cfc1cb25ad0c3d429385c805ec2a07ae
kudzanayi <kdzvairo@gmail.com>
2020-07-17 22:35:28 -0400

    buystock component

--- ttp_frontend/src/App.js
+++ ttp_frontend/src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import BuyStock from './Components/BuyStock'
+
+class App extends React.Component{
+
+ state = {
+   tickerSymbol:'',
+   quantity: 0
+ }
+
+  handleOnSubmitBuyButton = async (e, tickerSymbol, quantity) => {
+     e.preventDefault()
+     await this.setState({tickerSymbol:tickerSymbol, quantity:quantity})
+
+     fetch(`https://cloud.iexapis.com/stable/stock/${tickerSymbol}/company?token=pk_a1bdb3b5a0ef403ba560643968b4e8e4`)
+     .then(res => res.json())
+     .then(data => console.log(data))
+
+  }
 
 
 
-function App() {
-  return (
-    <div>
-      Hello World 
-    </div>
-  );
+  render(){
+    
+    return (
+      <>
+        <BuyStock handleOnSubmitBuyButton={this.handleOnSubmitBuyButton}/>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ ttp_frontend/src/Components/BuyStock.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+class BuyStock extends Component {
+    
+    state = {
+        tickerSymbol: '',
+        quantity: 0
+    }
+
+     handleChange = async(e) => {
+     await this.setState({[e.target.name]:e.target.value});
+      console.log(this.state)
+    }
+
+
+
+    render() {
+
+        const {tickerSymbol, quantity} = this.state
+        
+        return (
+          <>
+          <form action="" onSubmit={((e) => this.props.handleOnSubmitBuyButton(e, tickerSymbol, quantity))}>
+            <input type="text" name="tickerSymbol" value={tickerSymbol} onChange={e => this.handleChange(e)}/>
+            <input type="number" name="quantity" value={quantity} onChange={(e) => this.handleChange(e)}/>
+            <button>Buy</button>
+          </form>  
+          </>
+        )
+    }
+}
+
+export default BuyStock
